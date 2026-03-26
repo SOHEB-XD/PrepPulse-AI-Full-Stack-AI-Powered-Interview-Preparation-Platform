2990b3f6eb7575edfbb4e2097ae8552cb6436e5f
SOHEB... <112369546+SOHEB-XD@users.noreply.github.com>
2026-03-26 22:27:34 +0530

    Use environment variable for server port

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+const port = process.env.PORT || 3000
 connectToDB()
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3000")
 })
