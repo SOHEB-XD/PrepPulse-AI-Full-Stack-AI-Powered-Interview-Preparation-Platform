a5f44dd9e6559bf350ffa4444c3c6b540c437f6e
SOHEB... <112369546+SOHEB-XD@users.noreply.github.com>
2026-03-27 12:18:40 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,15 +38,16 @@ async function registerUserController(req, res) {
 
     const token = jwt.sign(
         { id: user._id, username: user.username },
-        process.env.JWT_SECRET,
-        { httpOnly: true,
-          secure: true,      // MUST be true for cross-site
-          sameSite: 'none',  // MUST be 'none' for cross-site
-          maxAge: 24 * 60 * 60 * 1000 // e.g., 1 day
-        }
+        process.env.JWT_SECRET
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, 
+              {
+                 httpOnly: true,
+                 secure: true,      // MUST be true for cross-site
+                 sameSite: 'none',  // MUST be 'none' for cross-site
+                 maxAge: 24 * 60 * 60 * 1000 // e.g., 1 day
+            })
 
 
     res.status(201).json({
@@ -92,7 +93,14 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token,
+              {
+                  httpOnly: true,
+                  secure: true,      // MUST be true for cross-site
+                  sameSite: 'none',  // MUST be 'none' for cross-site
+                  maxAge: 24 * 60 * 60 * 1000 // e.g., 1 day
+    })
+    
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
